9f9039f94eab8bc419bb211c024918c6cd6bf56a
vuvuzella <j.tabacii@gmail.com>
2016-07-28 04:58:14 +0800

    Added syncronosity

--- deck/emerge_deck.js
+++ deck/emerge_deck.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+
   Reveal.initialize({
     history: true,
     dependencies: [
@@ -14,6 +15,37 @@ window.onload = function() {
           return !!document.querySelector('[data-markdown]');
         }
       }
+      // {
+      //   src: 'plugins/socket.io-client/socket.io.js',
+      //   async:  true
+      // }
     ]
   });
+
+  // socket.io
+  var socket = io.connect('http://localhost:5000');
+
+  socket.on('message', function(data) {
+    console.log(data);
+    socket.emit('message', 'Welcome from the Front');
+  });
+
+  socket.on('slidechanged', function(data) {
+    console.log('server slidechanged received');
+    Reveal.slide(data.indexh, data.indexv, data.indexf);
+  });
+
+  var notifyServer = function(event) {
+    var data = {
+      indexv  : Reveal.getIndices().v,
+      indexh  : Reveal.getIndices().h,
+      indexf  : Reveal.getIndices().f || 0
+    };
+    socket.emit('slidechanged', data);
+    console.log(event);
+  }
+
+  Reveal.addEventListener('slidechanged', notifyServer);
+  Reveal.addEventListener('fragmentshown', notifyServer);
+  Reveal.addEventListener('fragmenthidden', notifyServer);
 };
--- server.js
+++ server.js
@@ -1,11 +1,57 @@
 var express = require('express');
-var path = require('path');
+var http    = require('http');
+var io      = require('socket.io');
+var path    = require('path');
+
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var masterUser = 'jeepers';
+var masterPass = 'paramaybaon';
 
 app.use(express.static(path.join(__dirname,'/deck')));
+app.set('port', process.env.PORT || 5000);
+var port = app.get('port');
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running in port', app.get('port'));
+// var server = http.createServer(app).listen(port, function() {
+//   console.log('Express server listening to port', port);
+// });
+
+var server = http.Server(app);
+var ioListener = io(server);
+
+server.listen(port, function() {
+  console.log('Express app listening to port', port);
 });
+
+var onMessage = function(data) {
+  console.log('message received');
+  console.log(data);
+}
+
+var onSlideChanged = function(data) {
+  console.log('slidechanged received');
+  console.log(data);
+  ioListener.broadcast.emit('slidechanged', data);
+}
+
+ioListener.on('connect', function(socket) {
+  console.log('connected!');
+  socket.emit('message', 'Welcome from back');
+
+  socket.on('message', function(data) {
+    console.log('message received');
+    console.log(data);
+    // ioListener.broadcast.emit('slidechanged', data);
+  });
+
+  socket.on('slidechanged', function(data) {
+    console.log('slidechanged received');
+    socket.broadcast.emit('slidechanged', data);
+  });
+});
+
+
+// ioListener.on('fragmentshown', function(data) {
+//   console.log('fragmentshown received');
+//   io.broadcast.emit('fragmentshown', data);
+// });
